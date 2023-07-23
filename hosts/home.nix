0248152c32c8c020509c7c15ab2163ce90bc048b
Kent Delante <leftybournes@pm.me>
2023-07-23 19:14:25 +0800

    Organize user packages

--- hosts/home.nix
+++ hosts/home.nix
@@ -9,24 +9,43 @@
     stateVersion = "23.05";
 
     packages = with pkgs; [
-      cargo
+      # cli tools
       chezmoi
-      clippy
       docker-compose
       elixir
+      elixir_ls
       fd
       flatpak-builder
+      hugo
       php
+      phpactor
       phpPackages.composer
       python3
       python3Packages.pip
       nodejs
       ripgrep
       ruby
+      unzip
+      yt-dlp
+      zip
+      zola
+
+      # desktop apps
+      adw-gtk3
+      amberol
+      celluloid
+      fragments
+      gnome-builder
+      gnome-connections
+      gnome-podcasts
+      mpv
+      virt-manager
+
+      # rust
+      cargo
+      clippy
       rustc
       rust-analyzer
-      unzip
-      zip
     ];
   };
 }
