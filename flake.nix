eb7fa93d742a9e980bebcea919e2e07cf9bb4fe7
Kent Delante <kent@kentdelante.com>
2025-01-20 08:07:05 +0800

    Remove cosmic and switch back to gnome

--- flake.nix
+++ flake.nix
@@ -3,12 +3,9 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    nixpkgs.follows = "nixos-cosmic/nixpkgs";
-
-    nixos-cosmic.url = "github:lilyinstarlight/nixos-cosmic";
   };
 
-  outputs = inputs @ { self, nixpkgs, nixos-cosmic, ... }:
+  outputs = inputs @ { self, nixpkgs, ... }:
     let
       system = "x86_64-linux";
       user = "vader";
@@ -21,22 +18,14 @@
           specialArgs = {
             # hostname = "endor";
             inherit (nixpkgs) lib;
-            inherit inputs nixpkgs user username nixos-cosmic;
+            inherit inputs nixpkgs user username;
           };
 
           inherit system;
 
           modules = [
-            {
-              nix.settings = {
-                substituters = [ "https://cosmic.cachix.org/" ];
-                trusted-public-keys = [ "cosmic.cachix.org-1:Dya9IyXD4xdBehWjrkPv6rtxpmMdRel02smYzA85dPE=" ];
-              };
-            }
-            nixos-cosmic.nixosModules.default
             ./common.nix
-            # ./gnome.nix
-            ./cosmic.nix
+            ./gnome.nix
             ./flatpak.nix
             ./hosts/endor
           ];
